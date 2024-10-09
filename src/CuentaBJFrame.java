a39fed52c29dc4ece89924e782d2219fab67c1db
gio10herrera <herrerafontalvo@outlook.com>
2024-10-08 23:00:42 -0500

    declaracion de variables

---
+++ src/CuentaBJFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class CuentaBJFrame {
+    static JLabel lblNumCuenta, lblNombreCliente, lblSaldo, lblResultado;
+    static JTextField txtNumCuenta, txtNombreCliente, txtSaldo;
+    static JTextArea txtAreaResultado;
+    static JButton btnDesplegarCuenta;
+    static JFrame frame;
+    static JPanel panelPadre, firstSonPanel, secondSonPanel, thirdSonPanel;
+
+    public static void main(String[] args) {
+        inicializarJFrame();
+    }
+
+    private static void inicializarJFrame() {
+    }
+}
