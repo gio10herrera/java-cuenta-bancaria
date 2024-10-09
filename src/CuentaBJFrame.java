55ab5d82029e34d5af63d111c3e8b64b879ca3d2
gio10herrera <herrerafontalvo@outlook.com>
2024-10-08 23:05:53 -0500

    inicializar componentes terminado

--- src/CuentaBJFrame.java
+++ src/CuentaBJFrame.java
@@ -10,7 +10,27 @@ public class CuentaBJFrame {
 
     public static void main(String[] args) {
         inicializarJFrame();
+        inicializarComponentes();
         frame.setVisible(true);
+    }
+
+    private static void inicializarComponentes() {
+        lblNumCuenta = new JLabel("Numero de Cuenta");
+        lblNumCuenta.setHorizontalAlignment(SwingConstants.CENTER);
+        lblNombreCliente = new JLabel("Nombre Cliente");
+        lblNombreCliente.setHorizontalAlignment(SwingConstants.CENTER);
+        lblSaldo = new JLabel("Saldo");
+        lblSaldo.setHorizontalAlignment(SwingConstants.CENTER);
+        lblResultado = new JLabel("Resultado");
+        lblResultado.setHorizontalAlignment(SwingConstants.CENTER);
+        txtNumCuenta = new JTextField();
+        txtNumCuenta.setHorizontalAlignment(SwingConstants.CENTER);
+        txtNombreCliente = new JTextField();
+        txtNombreCliente.setHorizontalAlignment(SwingConstants.CENTER);
+        txtSaldo = new JTextField();
+        txtSaldo.setHorizontalAlignment(SwingConstants.CENTER);
+        txtAreaResultado = new JTextArea();
+        btnDesplegarCuenta = new JButton("Desplegar Cuenta");
     }
 
     private static void inicializarJFrame() {
